7e0afcba5f33018c0bf392ff9b5252ac7ba30f51
RajNandini811 <rajkop21@gmail.com>
2024-08-27 17:06:47 +0530

    created auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/authentication/components/login/login.component';
+import { RegisterComponent } from './modules/authentication/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
