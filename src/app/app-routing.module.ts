b964b1e4c8d98f0beafc362619f7a0ac154bf87c
RajNandini811 <rajkop21@gmail.com>
2024-09-02 14:33:27 +0530

    Home page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/authentication/components/login/login.component';
 import { RegisterComponent } from './modules/authentication/components/register/register.component';
+import { HomeComponent } from './modules/home/components/home/home.component';
 
 const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },  
- 
+  { path: 'home', component: HomeComponent },
+
 ];
 
 @NgModule({
